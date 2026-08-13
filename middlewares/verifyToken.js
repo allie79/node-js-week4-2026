9f425bf80f1a414c16eee5e7dba0ac56ce035eaf
allie79 <yating719@gmail.com>
2026-08-14 00:52:17 +0800

    完成第四週作業

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -21,6 +21,18 @@ const jwt = require('jsonwebtoken');
  */
 const verifyToken = function (req, res, next) {
   /* 作答區 */
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ status: 'false', message: '請先登入' });
+  }
+  const token = authHeader.split(' ')[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ status: 'false', message: 'Token 無效或已過期' });
+  }
 };
 
 module.exports = verifyToken;
